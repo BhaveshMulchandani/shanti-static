02956a054e3913de23598f192012cc945da2c1b0
BhaveshMulchandani <bhaveshmulchandani330@gmail.com>
2026-06-18 14:48:25 +0530

    Refactor code structure for improved readability and maintainability

--- routes/web.js
+++ routes/web.js
@@ -6,6 +6,14 @@ const webController = require("../controllers/web.controller");
 
 router.get("/dental", webController.renderDentalPage);
 
+router.get("/booking", (req, res) => {
+  res.render("booking");
+});
+
+router.get("/booking-success", (req, res) => {
+  res.render("booking-success");
+});
+
 router.get("/services/:slug", async (req, res) => {
   try {
     const service = await Service.findOne({
